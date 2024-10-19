ccdc17d436ba82a1344e4c741c12f78b3b7296f7
HaoDong-Xia <3394797836@qq.com>
2024-10-19 21:24:12 +0800

    一阶段提交

--- kernel/boot/main.c
+++ kernel/boot/main.c
@@ -1,9 +1,39 @@
 #include "riscv.h"
 #include "lib/print.h"
+#include "lib/str.h"
+#include "mem/pmem.h"
+#include "mem/vmem.h"
+#include "trap/trap.h"
 
 volatile static int started = 0;
 
 int main()
 {
+    int cpuid = r_tp();
+
+    if(cpuid == 0) {
+        
+        print_init();
+        printf("cpu %d is booting!\n", cpuid);
+
+        pmem_init();
+        kvm_init();
+        kvm_inithart();
+        trap_kernel_init();
+        trap_kernel_inithart();
+
+        __sync_synchronize();
+        started = 1;
+
+    } else {
+
+        while(started == 0);
+        
+        __sync_synchronize();
+        printf("cpu %d is booting!\n", cpuid);
+
+        kvm_inithart();
+        trap_kernel_inithart();
+    }
     while (1);    
 }
