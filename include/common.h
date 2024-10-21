cbae7a622449407aa2dcf3a3287412dca481a12d
HaoDong-Xia <3394797836@qq.com>
2024-10-21 17:55:01 +0800

    补充common.h

--- include/common.h
+++ include/common.h
@@ -28,4 +28,12 @@ typedef enum {false = 0, true = 1} bool;
 #define PGSIZE 4096    // 物理页大小
 
 
+// 帮助
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b)) // 取大
+#define MIN(a,b) ((a) < (b) ? (a) : (b)) // 取小
+
+#define ALIGN_UP(addr,refer) (((addr) + (refer) - 1) & ~((refer) - 1)) // 向上对齐
+#define ALIGN_DOWN(addr,refer) ((addr) & ~((refer) - 1)) // 向下对齐
+
 #endif
