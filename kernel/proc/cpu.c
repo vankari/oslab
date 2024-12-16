a2944f5cf8544062518cb32e302539bd51861170
vankari <760238843@qq.com>
2024-12-16 23:45:20 +0800

    修改：     kernel/boot/main.c 	修改：     kernel/boot/start.c 	修改：     kernel/lib/print.c 	修改：     kernel/lib/spinlock.c 	修改：     kernel/proc/cpu.c lab-1 complete

--- kernel/proc/cpu.c
+++ kernel/proc/cpu.c
@@ -3,12 +3,16 @@
 
 static cpu_t cpus[NCPU];
 
+//此cpu的cpu struct 需要中断关闭
 cpu_t* mycpu(void)
 {
-
+    int id = mycpuid();
+    struct cpu *c = &cpus[id];
+    return c;
 }
-
+//返回线程的cpu id 需要关闭中断
 int mycpuid(void) 
 {
-
+    int id = r_tp();
+    return id;
 }
